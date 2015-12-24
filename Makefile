4108c98c322370054cd075fca9a5f7c259987f45
Matus Tejiscak <ziman@functor.sk>
2015-12-24 10:18:29 +0100

    Add Makefile.

---
+++ Makefile
@@ -0,0 +1,2 @@
+list.js: srt/*.srt compile.py
+	./compile.py
--- list.js
+++ list.js
@@ -1,20 +1,74 @@
 window.SRTS = [
-  "_test.srt",
-  "Drei.Haselnuesse.fuer.Aschenbroedel.(Remastered).DVDRip.German.AC3.by.forever-english.srt",
-  "jak-dostat-tatinka-do-polepsovny.eng.srt",
-  "Jak vytrhnout velrybe stolicku (1977).eng.srt",
-  "jakvyvest-pivo-eng.srt",
-  "marecku.podejte.mi.pero.srt",
-  "Pelisky-eng.srt",
-  "Pelisky.1999.1080p.BluRay.DTS-HDMA.x264.srt",
-  "perinbaba.txt",
-  "popelka.srt",
-  "Princezna se zlatou hvezdou.srt",
-  "s-tebou-me-bavi-svet.srt",
-  "Three Wishes Of Cinderella (Tri Orisky Pro Popelku) (1973) Kinobox.srt",
-  "vesnicko.eng.srt",
-  "vesnicko.ma.strediskova.cd1-xvidcz.english.sub",
-  "vesnicko.ma.strediskova.cd1-xvidcz.sub",
-  "vesnicko.ma.strediskova.cd2-xvidcz.sub",
-  "Vesnicko Ma Strediskova (1985).srt"
-];
+  {
+    "events": [],
+    "name": "vesnicko.ma.strediskova.cd2-xvidcz.sub"
+  },
+  {
+    "events": [],
+    "name": "Drei.Haselnuesse.fuer.Aschenbroedel.(Remastered).DVDRip.German.AC3.by.forever-english.srt"
+  },
+  {
+    "events": [],
+    "name": "Pelisky.1999.1080p.BluRay.DTS-HDMA.x264.srt"
+  },
+  {
+    "events": [],
+    "name": "jakvyvest-pivo-eng.srt"
+  },
+  {
+    "events": [],
+    "name": "Three Wishes Of Cinderella (Tri Orisky Pro Popelku) (1973) Kinobox.srt"
+  },
+  {
+    "events": [],
+    "name": "Jak vytrhnout velrybe stolicku (1977).eng.srt"
+  },
+  {
+    "events": [],
+    "name": "marecku.podejte.mi.pero.srt"
+  },
+  {
+    "events": [],
+    "name": "popelka.srt"
+  },
+  {
+    "events": [],
+    "name": "perinbaba.txt"
+  },
+  {
+    "events": [],
+    "name": "vesnicko.ma.strediskova.cd1-xvidcz.sub"
+  },
+  {
+    "events": [],
+    "name": "Pelisky-eng.srt"
+  },
+  {
+    "events": [],
+    "name": "jak-dostat-tatinka-do-polepsovny.eng.srt"
+  },
+  {
+    "events": [],
+    "name": "_test.srt"
+  },
+  {
+    "events": [],
+    "name": "vesnicko.eng.srt"
+  },
+  {
+    "events": [],
+    "name": "s-tebou-me-bavi-svet.srt"
+  },
+  {
+    "events": [],
+    "name": "Vesnicko Ma Strediskova (1985).srt"
+  },
+  {
+    "events": [],
+    "name": "vesnicko.ma.strediskova.cd1-xvidcz.english.sub"
+  },
+  {
+    "events": [],
+    "name": "Princezna se zlatou hvezdou.srt"
+  }
+]
